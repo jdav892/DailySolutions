062a0721f0aaaf4c6165bb236375906e6a21a3d2
jdav892 <jadavila9@gmail.com>
2024-06-25 12:44:21 -0400

    simpler solution

--- validate_regex.py
+++ validate_regex.py
@@ -1,7 +1,11 @@
 import re
 def validate_code(code):
-    check_code = re.search("^[123]", code)
+    check_code = re.search("^[123]", str(code))
     if check_code:
         return True
     else:
         return False
+    
+    
+# def validcate_code(code):
+#   return str(code).startswith(('1', '2', '3'))
