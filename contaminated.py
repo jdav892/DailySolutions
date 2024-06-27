2f3f23d2b347c662200ab83c5e3a6830ac44f9a9
jdav892 <jadavila9@gmail.com>
2024-06-26 20:24:05 -0400

    seen before

---
+++ contaminated.py
@@ -0,0 +1,8 @@
+def contamination(text, char):
+    mut_string = ""
+    if text == "" or char == "":
+        return mut_string
+    for i in text:
+        mut_string += char
+    return mut_string
+    
