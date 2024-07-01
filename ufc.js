49c4388a838688737d206a91e7746c61b59728ca
jdav892 <jadavila9@gmail.com>
2024-07-01 13:57:09 -0400

    ufc

---
+++ ufc.js
@@ -0,0 +1,4 @@
+var quote = function(fighter) {
+  let fighterCase = fighter.toLowerCase()
+  return fighterCase === 'george saint pierre' ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
