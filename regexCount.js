f52c5259e0317df4348016385b647e624507217b
jdav892 <jadavila9@gmail.com>
2024-07-06 23:47:24 -0400

    anotha one

---
+++ regexCount.js
@@ -0,0 +1,9 @@
+function lowerCaseCount(str){
+    let count = 0
+    for(let i = 0; i < str.length; i++){
+        if(/[a-z]/.test(str[i])){
+            count ++
+        }
+    }
+    return count
+}
