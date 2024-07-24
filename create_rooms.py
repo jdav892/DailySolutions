9d9cd16f490411e35432040c865c8e37fd408a01
jdav892 <jadavila9@gmail.com>
2024-07-24 14:20:48 -0400

    clean

---
+++ create_rooms.py
@@ -0,0 +1,18 @@
+rooms = {
+    "One" : {
+        "name": "One",
+        "description" : "Master",
+        "completed" : True
+        
+    },
+    "Two" : {
+        "name" : "Two",
+        "description": "Guest",
+        "completed" : True
+    },
+    "Three" : {
+        "name": "Three",
+        "description": "Guest",
+        "completed": False
+    }
+}
