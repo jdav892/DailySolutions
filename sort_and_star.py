cdc85d404ae8c177b4d8deb092c990acb09fb5f3
jdav892 <jadavila9@gmail.com>
2024-08-14 18:31:52 -0400

    cleaner solution

--- sort_and_star.py
+++ sort_and_star.py
@@ -3,3 +3,5 @@ def two_sort(array):
     first = sorted_string[0]
     formatted = "***".join(first)
     return formatted
+
+# return "***".join(sorted(arr)[0]) also works, same with "***".join(min(lst))
