b7bb8da94f432d3781dedd8a951b67c10077eaf6
jdav892 <jadavila9@gmail.com>
2024-08-21 12:44:04 -0400

    okay i guess

--- title_case.py
+++ title_case.py
@@ -17,3 +17,8 @@ def title_case(title, minor_words=''):
         new.append(word.title())
         
     return ' '.join(new)
+
+#def title_case(title, minor_words=''):
+# title = title.capitalize().split()
+# minor_words = minor_words.lower().split()
+# return ' '.join([word if word in minor_words else word.capitalize() for word in title])
