a026b51da89dc65f26c240559aa1d47393b4d221
jdav892 <jadavila9@gmail.com>
2024-09-06 15:16:59 -0400

    my special

---
+++ special_number_five.py
@@ -0,0 +1,8 @@
+def special_number(number):
+    specials = [0 ,1 ,2, 3, 4, 5]
+    nums = [int(num) for num in str(number)]
+    for digit in nums:
+        if digit not in specials:
+            return "NOT!!"
+    return "Special!!"
+
