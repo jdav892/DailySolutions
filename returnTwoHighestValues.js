a64a5be7f9607743af3b2a990947bf8a8931b17f
jdav892 <jadavila9@gmail.com>
2024-09-07 18:24:45 -0400

    sorting sets

---
+++ returnTwoHighestValues.js
@@ -0,0 +1,11 @@
+function twoHighest(arr){
+    if(arr === null){
+        return []
+    }
+    let sortedArr = [...new Set(arr)].sort((a, b) => b - a);
+    if(sortedArr.length > 1){
+        return [sortedArr[0], sortedArr[1]]
+    }else{
+        return [sortedArr[0]]
+    }
+}
