7cd2e3510db9ed00109eca4f5737ebab7f595c5d
jdav892 <jadavila9@gmail.com>
2024-09-15 22:30:25 -0400

    cleaner

--- linked_lists_push_build.py
+++ linked_lists_push_build.py
@@ -4,14 +4,9 @@ class Node:
         self.next = None
         
     def push(head, data):
-        new_node = Node(data)
-        new_node.next = head
-        return new_node
+        n = Node(data)
+        n.next = head
+        return n
     
     def build_one_two_three():
-        head = None
-        head = push(head, 3)
-        head = push(head, 2)
-        head = push(head, 1)
-        return head
-    
+        return push(push(Node(3), 2), 1)
