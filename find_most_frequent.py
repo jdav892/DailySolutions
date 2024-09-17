02d85c8214d0dcfc5dbae43745eccdb6b8c347b4
jdav892 <jadavila9@gmail.com>
2024-09-17 03:57:00 -0400

    clean

--- find_most_frequent.py
+++ find_most_frequent.py
@@ -1,6 +1,4 @@
 from collections import Counter
 def most_frequent_item_count(collection):
-    if not collection:
-        return 0
-    count = Counter(collection)
-    return max(count.values())
+    if collection:
+        return max([collection.count(item) for item in collection])
