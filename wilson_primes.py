18f3158bd116e775c619dcd4366b00c62efbbca9
jdav892 <jadavila9@gmail.com>
2024-09-18 17:59:24 -0400

    brute force that fails time out

--- wilson_primes.py
+++ wilson_primes.py
@@ -1,3 +1,18 @@
 def am_i_wilson(n):
     known = [5, 13, 563]
     return n in known
+
+
+
+#import math
+#def another_wilson(n):
+#    if n < 2:
+#        return False
+#    for i in range(2, int(math.sqrt(n)) + 1):
+#        if n % i == 0:
+#            return False
+#    
+#    facto = math.factorial(n - 1)
+#    expression = facto + 1
+#    
+#    return expression % (n * n) == 0
