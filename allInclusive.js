84a1f75459baca703e2b7148da28bef9f5d97024
jdav892 <jadavila9@gmail.com>
2024-09-26 18:50:39 -0400

    fixed given case

--- allInclusive.js
+++ allInclusive.js
@@ -1,7 +1,4 @@
 function containsAllRots(strng, arr){
-    if(strng === ""){
-        return true
-    }
     for(let i = 0; i < strng.length; i++){
         const rotation = strng.slice(i) + String.slice(0, i)
         if(!arr.includes(rotation)){
