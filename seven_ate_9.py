2e6d71562f15b05bdf530f822a8ae959f4fc5247
jdav892 <jadavila9@gmail.com>
2024-09-27 23:39:37 -0400

    came up with my own thing rather than regex

---
+++ seven_ate_9.py
@@ -0,0 +1,10 @@
+def seven_ate_9(str_):
+    num_str = list(num_str)
+    
+    num = 0
+    while num < len(str_) - 2:
+        if num_str[num] == '7' and num_str[num + 1] == '9' and num_str[num + 2]:
+            del num_str[num + 1]
+        else:
+            num += 1
+    return ''.join(num_str) 
