7fc1af434a71b1091cccbc4417957381921b2056
jdav892 <jadavila9@gmail.com>
2024-09-30 11:47:31 -0400

    better

--- countTheDigit.js
+++ countTheDigit.js
@@ -11,3 +11,10 @@ function nbDig(n, d){
     return count
 }
 
+//function nbDig(n, d){
+//    let num = '';
+//    for(let i = 0; i <= n; i++){
+//        num += Math.pow(i, 2);
+//    }
+//    return num.split(d).length - 1
+//}
