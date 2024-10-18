2c546ef1a673534c414a90da8c2440e607671f2d
jdav892 <jadavila9@gmail.com>
2024-10-18 12:26:07 -0400

    splits a string of words by whitespace, declares a variable to store longest word value, iterates through the list of split words and updates the longest variable as it iterates to be returned at the end

---
+++ string_of_words.py
@@ -0,0 +1,9 @@
+def longest_word(string_of_words):
+    words = string_of_words.split()
+    longest = ""
+    for word in words:
+        if len(word) >= len(longest):
+            longest = word
+    return longest
+    
+        
