1b099735162be8f93fbe776ac935c078cf177ac7
jdav892 <jadavila9@gmail.com>
2024-10-19 11:11:18 -0400

    fixes errors with single character strings

--- band_name_gen.py
+++ band_name_gen.py
@@ -1,4 +1,7 @@
 def band_name_generator(name):
+    if len(name) < 2:
+        return name.capitalize()
+    
     if name[-1] == name[0]:
         return name[:-1].capitalize() + name[0:]
     else:
