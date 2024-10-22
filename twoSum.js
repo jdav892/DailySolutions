f6fdf0151df8f8f126a5ab46e90bb4ee1162c7ea
jdav892 <jadavila9@gmail.com>
2024-10-22 00:36:41 -0400

    assigns key value pairs using the map object in javascript to assign key value pairs to then use the difference of the target and value indexes to find the target pair to be returned

---
+++ twoSum.js
@@ -0,0 +1,15 @@
+var twoSum = function(nums, target){
+    const map = new Map()
+    for(let i = 0; i < nums.length; i++){
+        const num = nums[i]
+        const val = target - num
+        const sumIndex = map.get(val)
+
+        const isTarget = map.has(val)
+        if(isTarget){
+            return [i, sumIndex]
+        }
+        map.set(num, i)
+    }
+    return [-1, -1]
+}
