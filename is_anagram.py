f585a41e8e45a8f51668921a966dbe64ae9980aa
jdav892 <jadavila9@gmail.com>
2024-10-25 14:29:13 -0400

    fix: changed if statement that compares the c value between maps

--- is_anagram.py
+++ is_anagram.py
@@ -14,7 +14,7 @@ class Solution(object):
             counterS[s[i]] = 1 + counterS.get(s[i], 0)
             counterT[t[i]] = 1 + counterT.get(t[i], 0)
         for c in counterS:
-            if counterS[c] != counterT[c]:
+            if counterS[c] != counterT.get(c, 0):
                 return False
         return True
         
