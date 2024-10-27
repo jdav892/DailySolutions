0b58d549abab4bec605ed844d3f1058b322f0997
jdav892 <jadavila9@gmail.com>
2024-10-27 19:37:06 -0400

    converts the number to a string, compares itself to its reverse and returns a boolean

---
+++ palindrome_number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        str_num = str(x)
+        
+        return str_num == str_num[::-1]
