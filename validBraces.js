215b0d827c280f06c69543b144dca89b2e422586
jdav892 <jadavila9@gmail.com>
2024-10-29 14:54:20 -0400

    properly executes hashing of the parenthesis and checks if all are properly closed

--- validBraces.js
+++ validBraces.js
@@ -1,18 +1,28 @@
-function validBraces(braces){
+function validBraces(s){
     const stack = []
     const braceMap = {
-        '(' : ')',
-        '[' : ']',
-        '{' : '}'
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
     };
-    for (let brace in braces){
-        if (braceMap[brace]){
-            stack.push(braceMap[brace])
-        }else{
-            if (stack.pop() !== brace){
+    const openingBrackets = ['(', '[', '{'];
+    const closingBrackets = [')', ']', '}'];
+
+    for(let char of s){
+        if(openingBrackets.includes(char)){
+            stack.push(char);
+        }else if(closingBrackets.includes(char)){
+            if(stack.length === 0 || stack.pop() !== braceMap[char]){
                 return false;
             }
         }
     }
     return stack.length === 0;
+
 }
+
+console.log(validBraces("()"));
+console.log(validBraces("{}{{{}"));
+console.log(validBraces("[]"));
+console.log(validBraces("()))((()()(("));
+console.log(validBraces("[({})]"))
