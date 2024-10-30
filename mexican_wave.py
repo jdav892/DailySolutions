f2d74b792725b843747e56c585c06385fd17846e
jdav892 <jadavila9@gmail.com>
2024-10-30 16:24:17 -0400

    creates a stack of letters and iterates through the stack to then create a new array with each version of the string with a capitalized char in each place

---
+++ mexican_wave.py
@@ -0,0 +1,19 @@
+def wave(people):
+    if people == "":
+        return ""
+    else:
+        people = people.lower()
+        waves = []
+        for i, char in enumerate(people):
+            if char == " ":
+                continue
+            else:
+                waves.append(people[:i] + people[i].upper() + people[i+1:])
+        return waves
+            
+            
+            
+        
+        
+        
+    
