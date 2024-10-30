64bd7c4bb8c90a5e841703576310c5d79eaafe31
jdav892 <jadavila9@gmail.com>
2024-10-30 18:05:39 -0400

    fix

--- lengthOfTheLastWord.js
+++ lengthOfTheLastWord.js
@@ -2,9 +2,6 @@ var lengthOfLastWord = function(s) {
     let newStr = s.trim();
     let index = newStr.length;
     let length = 0;
-    while(newStr[index] == " "){
-        index -= 1;
-    }
 
     while(index >= 0 && newStr[index] != " "){
         length += 1;
