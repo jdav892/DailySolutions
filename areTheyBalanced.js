2a1f480be12904364b5eb8548130bca2bcd78f52
jdav892 <jadavila9@gmail.com>
2024-10-31 14:12:49 -0400

    changed if statement in loops to ternaries

--- areTheyBalanced.js
+++ areTheyBalanced.js
@@ -3,18 +3,10 @@ function balance(left, right){
     let rightSum = 0;
 
     for(let i = 0; i < left.length; i++){
-        if(left[i] === "!"){
-            leftSum += 2
-        }else{
-            leftSum += 3
-        }
+        left[i] === "!" ? leftSum += 2 : leftSum += 3;
     }
     for(let j = 0; j < right.length; j++){
-        if(right[j] === "!"){
-            rightSum += 2
-        }else{
-            rightSum += 3
-        }
+        right[j] === "!" ? rightSum += 2 : rightSum += 3;
     }
 
     if(leftSum > rightSum){
