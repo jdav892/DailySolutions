03d3d5af848845037858da77f6292254cbaa091f
jdav892 <jadavila9@gmail.com>
2024-10-31 16:17:47 -0400

    creates a hashmap of the roman numerals and their values, we iterate through them assigning a value to each key and then initializing a counter for the amount symbols that need to be returned, we then decrement from num based on what shouldn't be there and we add to result the values that occur based on the count and return the result

---
+++ intToRoman.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+  const roman = {
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C" : 100,
+    "XC": 90,
+    "L" : 50,
+    "XL" : 40,
+    "X" : 10,
+    "IX" : 9,
+    "V" : 5,
+    "IV" : 4,
+    "I" : 1,
+  };
+  let result = ""
+  for(const value of Object.keys(roman)){
+    let count = Math.floor(num / roman[value]);
+    num -= count * roman[value];
+    result += value.repeat(count)
+  }
+    return result
+};
