3a6f3f33a7805233bf729847b18d185737051560
jdav892 <jadavila9@gmail.com>
2024-11-06 13:06:29 -0500

    applies includes method to passed string to check if one of three substrings exist and return true if one of them meets the condition

---
+++ aStrangeTripToTheMarket.js
@@ -0,0 +1,7 @@
+function isLocknessMonster(s){
+    if(s.includes("tree fiddy") || s.includes("3.50") || s.includes("three fifty")){
+        return true
+      }else{
+        return false
+      }  
+}
