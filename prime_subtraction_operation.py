abc1600c1095b7c29214bc626aa6ba8fc02074cb
jdav892 <jadavila9@gmail.com>
2024-11-11 16:50:50 -0500

    defines a prime number, the using a prime number that is less than nums[i] we return a boolean if we can make a strictly increasing array using the operation

---
+++ prime_subtraction_operation.py
@@ -0,0 +1,25 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        def is_prime(n):
+            for f in range(2, int(sqrt(n)) + 1):
+                if n % f == 0:
+                    return False
+            return True
+        
+        primes = [0, 0]
+        for i in range(2, max(nums)):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(primes[i - 1])
+                
+        prev = 0
+        for n in nums:
+            upper_bound = n - prev
+            largest_p = primes[upper_bound - 1]
+            if n - largest_p <= prev:
+                return False
+            prev = n - largest_p
+            
+        return True   
+             
