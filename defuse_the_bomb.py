7d610c4ef27c6064c04b20680e336958d8d8ccc4
jdav892 <jadavila9@gmail.com>
2024-11-19 00:00:21 -0500

    actual sliding window solution for defuse the bomb leetcode problem

--- defuse_the_bomb.py
+++ defuse_the_bomb.py
@@ -1,15 +1,35 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
+    ##    N = len(code)
+    ##    
+    ##    res = [0] * N
+    ##    
+    ##    for i in range(N):
+    ##        if k > 0:
+    ##            for j in range(i + 1, i + 1 + k):
+    ##                res[i] += code[j % N]
+    ##        elif k < 0:
+    ##            for j in range(i - 1, i - 1 - abs(k), -1):
+    ##                res[i] += code[j % N]
+    ##
+    ##    return res
+    
         N = len(code)
-        
         res = [0] * N
         
-        for i in range(N):
-            if k > 0:
-                for j in range(i + 1, i + 1 + k):
-                    res[i] += code[j % N]
-            elif k < 0:
-                for j in range(i - 1, i - 1 - abs(k), -1):
-                    res[i] += code[j % N]
-    
+        l = 0
+        current = 0
+        for i in range(N + abs(k)):
+            current += code[i % N]
+            
+            if i - l + 1 > abs(k):
+                current -= code[l % N]
+                l = (l + 1) % N
+            
+            if i - l + 1 == abs(k):
+                if k > 0:
+                    res[(l - 1) % N] = current
+                elif k < 0:
+                    res[(i + 1) % N] = current    
+            
         return res
