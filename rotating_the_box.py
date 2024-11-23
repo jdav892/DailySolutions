624c7b88e84c1950bfddd8293ebcc5b5ea5c027b
jdav892 <jadavila9@gmail.com>
2024-11-22 20:35:11 -0500

    a sceond solution that more so applies rotations rather than manipulating arrays

--- rotating_the_box.py
+++ rotating_the_box.py
@@ -2,23 +2,18 @@ class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         X, Y = len(box), len(box[0])
         
+        res = [["."] * X for _ in range(Y)]
+        
         for r in range(X):
             i = Y - 1
             for c in reversed(range(Y)):
                 if box[r][c] == "#":
-                    box[r][c], box[r][i] = box[r][i], box[r][c]
+                    res[i][X - r - 1] = "#" 
                     i -= 1
                 elif box[r][c] == "*":
+                    res[c][X - r - 1] = "*"
                     i = c - 1
                     
-        res = []
-       
-        for c in range(Y):
-            col = []
-            for r in reversed(range(X)):
-                col.append(box[r][c])
-            
-            res.append(col) 
         return res
                 
                 
