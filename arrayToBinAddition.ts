95c9338c1cec18e2fa07af89bac64d9e07d2adf0
jdav892 <jadavila9@gmail.com>
2024-11-23 01:47:09 -0500

    second solution more so reliant on method chaining, from filtering out non number values, reducing the values to a sum and converting to binary format with string type

--- arrayToBinAddition.ts
+++ arrayToBinAddition.ts
@@ -2,3 +2,16 @@ export function arr2bin(arr: any[]): string {
     const binSum = arr.reduce((acc, curr) => acc + (typeof curr === "number" ? curr : 0 ), 0);
     return binSum.toString(2); 
 }
+
+
+
+export function new2bin(arr: any[]): string {
+    return arr
+        .filter(index => typeof(index) === 'number')
+        .reduce(
+            (a, b) => a + b,
+            0
+        )
+        .toString()
+        ;
+}
