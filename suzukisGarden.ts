5eac5acd91de39915ff231026735f70626891b9d
jdav892 <jadavila9@gmail.com>
2024-11-24 22:24:27 -0500

    fixed conditional to match proper logic

--- suzukisGarden.ts
+++ suzukisGarden.ts
@@ -1,7 +1,7 @@
 export function rakeGarden(garden:string):string {
     let strArr: string[] = garden.split(" ");
     for(let i = 0; i < strArr.length; i++){
-        if(strArr[i] !== "gravel" || strArr[i] !== "rock"){
+        if(strArr[i] !== "gravel" && strArr[i] !== "rock"){
             strArr[i] = "gravel"
         }
     }
