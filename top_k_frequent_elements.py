e831b905eeeab7342b2eb1fdfe4cb3a0e35e990c
jdav892 <jadavila9@gmail.com>
2024-12-04 09:36:00 -0500

    feat: O(n) solution with bucket sort

--- top_k_frequent_elements.py
+++ top_k_frequent_elements.py
@@ -13,3 +13,29 @@ class Solution:
                 heapq.heappushpop(heap, (key, val))
         
         return [h[1] for h in heap]
+    #Time:O(n * log K), Space: O(n)
+    
+        """
+        O(n) solution:
+        n = len(nums)
+        counter = Counter(nums)
+        buckets = [0] * (n + 1)
+        
+        for num, freq,; in counter.items():
+            if buckets[freq] == 0:
+                buckets[freq] = [num]
+            else:
+            buckets[freq].append[num]
+            
+        ret = []
+        
+        for i in range(n, -1, -1):
+            if buckets[i] != 0:
+                ret.extend(buckets[i])
+            if len(ret) == k:
+                break
+        
+        return ret
+       
+       Time: O(n) Space: O(n) 
+        """
