5b49464dbc1aefc5238e6c4ca8a24dbdef4bc416
jdav892 <jadavila9@gmail.com>
2024-12-04 09:28:20 -0500

    feat: creates a dictionary to store key value pairs for frequency, then we push keys to a heap as we continue to look up through the dictionary we pop preceding values until there are only k elements within heap and perform a list comprehension of keys to be returned

---
+++ top_k_frequent_elements.py
@@ -0,0 +1,15 @@
+import heapq
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        counter = Counter(nums)
+        heap = []
+        
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (key, val))
+            else: 
+                heapq.heappushpop(heap, (key, val))
+        
+        return [h[1] for h in heap]
