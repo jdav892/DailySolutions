e20d4ec2db73202259f8d1e980a13f77ff9eed53
jdav892 <jadavila9@gmail.com>
2024-12-16 13:01:54 -0500

    feat: creates a min heap to dynimcally determine a minimum value and mulitply the matching index in result copy of given array and return result when we've stepped through the full range of k(number of operations)

---
+++ final_array_state_after_k_multiplication.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        result = [::]
+        
+        min_heap = [(n , i) for i, n in enumerate(nums)]
+        heapq.heapify(min_heap)
+        
+        for _ in range(k):
+            n, i = heapq.heappop(min_heap)
+            
+            result[i] *= multiplier
+            heapq.heappush(min_heap, (result[i], i))
+            
+        return result
