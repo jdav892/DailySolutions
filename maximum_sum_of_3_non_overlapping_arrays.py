1030acb11d68a6053d95adc20ebf70c0c781a865
jdav892 <jadavila9@gmail.com>
2024-12-29 11:07:42 -0500

    feat: recursively checks for subarrays to meet condition for max sum per 3 element length subarray

--- maximum_sum_of_3_non_overlapping_arrays.py
+++ maximum_sum_of_3_non_overlapping_arrays.py
@@ -1,23 +1,36 @@
 class Solution:
     def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
-        maximum, result = 0, []
+        k_sum = [sum(nums[:k])]
+        
+        k = 3
+        dp = {}
+        
+        for i in range(k, len(nums)):
+            k_sum.append(k_sum[-1] + nums[i] - nums[i - k])
+            
+        def get_max_sum(i, count):
+            if count == 3 or i > len(nums) - k:
+                return 0
+           #include
+            include = k_sum[i] + get_max_sum(i + k , count + 1)
+            
+            #skip 
+            skip = get_max_sum(i + 1, count)
+            dp[(i, count)] = max(include, skip)
+            return dp[(i, count)]
 
-        def backtrack(i , chosen):
-            if len(chosen) == 3 or i >= len(nums) - 1:
-                nonlocal maximum, result
-                current_sum = 0
-                for j in chosen:
-                    current_sum += sum(nums[j:j+k])
-                if current_sum > maximum:
-                    maximum = current_sum
-                    result = chosen[:]
-                return
-
-            chosen.append(i)
-            backtrack(i + k, chosen)
-            chosen.pop()
-
-            backtrack(i + 1, chosen)
-
-        backtrack(0, [])
-        return result
+        def get_indices():
+            i = 0
+            indices = []
+            while i <= len(nums) - k and len(indices) < 3:
+                include = k_sums[i] + get_max_sum(i + k, len(indices) + 1)
+                skip = get_max_sum(i + 1, len(indices))
+                if include >= skip:
+                    indices.append(i)
+                    i += k
+                else:
+                    i += 1
+            
+            return indices
+        
+        return get_indices 
