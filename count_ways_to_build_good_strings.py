a5a3fdfa7fc6085ea532e0d96f9f214543a2839f
jdav892 <jadavila9@gmail.com>
2024-12-30 12:19:37 -0500

    feat: bottom up approach that is slightly more efficient Time: O(n), Space: O(n)

--- count_ways_to_build_good_strings.py
+++ count_ways_to_build_good_strings.py
@@ -15,3 +15,13 @@ class Solution:
             return dp[length] % modifier
         
         return dfs(0)
+    
+            """
+           modifier = 10 ** 9 + 7
+           dp = {0 : 1}
+           
+            for i in range(1, high + 1):
+                dp[i] = (dp.get(i - one, 0) + dp.get(i - zero, 0)) % modifier
+            return sum([dp[i] for i in range(low ,high + 1)]) % mod
+            
+            """
