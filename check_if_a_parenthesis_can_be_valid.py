a8478f97dd146c0c15d4868bcb7e4cabc2b19b67
jdav892 <jadavila9@gmail.com>
2025-01-12 13:03:24 -0500

    feat: uses a greedy approach by incrementing a count and returning if we've found all matches through first pass, if not continue through a reversal and return a boolean

--- check_if_a_parenthesis_can_be_valid.py
+++ check_if_a_parenthesis_can_be_valid.py
@@ -1,30 +1,71 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        
+        #if len(s) % 2 == 1:
+        #    return False
+        #
+        #stack_locked = [] 
+        #stack_unlocked = []
+        #
+        #for i in range(len(s)):
+        #    if locked[i] == "0":
+        #        stack_unlocked.append(i)
+        #    elif s[i] == "(":
+        #        stack_locked.append(i)
+        #    else:
+        #        if stack_locked:
+        #            stack_locked.pop()
+        #        elif stack_unlocked:
+        #            stack_unlocked.pop()
+        #        else:
+        #            return False
+        #
+        #while stack_locked and stack_unlocked and stack_locked[-1] < stack_unlocked[-1]:
+        #    stack_locked.pop()
+        #    stack_unlocked.pop()
+        #if stack_locked:
+        #    return False
+        #
+        #return True
         if len(s) % 2 == 1:
             return False
         
-        stack_locked = [] 
-        stack_unlocked = []
+        locked_count = 0
+        unlocked_count = 0
         
         for i in range(len(s)):
             if locked[i] == "0":
-                stack_unlocked.append(i)
+                unlocked_count += 1
             elif s[i] == "(":
-                stack_locked.append(i)
+                locked_count += 1
             else:
-                if stack_locked:
-                    stack_locked.pop()
-                elif stack_unlocked:
-                    stack_unlocked.pop()
+                if locked_count:
+                    locked_count -= 1
+                elif unlocked_count:
+                    unlocked_count -= 1
+                else:
+                    return False
+                
+        if locked_count == 0:
+            return True
+        
+        locked_count = 0
+        unlocked_count = 0
+        
+        for i in reversed(range(len(s))):
+            if locked[i] == "0":
+                unlocked_count += 1
+            elif s[i] == ")":
+                locked_count += 1
+            else:
+                if locked_count:
+                    locked_count -= 1
+                elif unlocked_count:
+                    unlocked_count -= 1
                 else:
                     return False
         
-        while stack_locked and stack_unlocked and stack_locked[-1] < stack_unlocked[-1]:
-            stack_locked.pop()
-            stack_unlocked.pop()
-        if stack_locked:
-            return False
+        return True    
+            
+                
         
-        return True
                 
