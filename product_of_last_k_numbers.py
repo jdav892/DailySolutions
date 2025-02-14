e864636bf50974faa99fe5e09ffe16162b173c07
jdav892 <jadavila9@gmail.com>
2025-02-13 21:06:11 -0500

    feat: creates a python class to multiply values and appended to the array and returns the product of the numbers in range k and returns the value

---
+++ product_of_last_k_numbers.py
@@ -0,0 +1,22 @@
+class ProductOfNumbers:
+    
+    def __init__(self):
+        self.products = []
+        self.product = 1
+        
+        
+    def add(self, num: int) -> None:
+        if num:
+            self.product *= num
+            self.products.append(self.product)
+        else:
+            self.products = []
+            self.product = 1
+            
+    def getProduct(self, k: int) -> int:
+        if len(self.products) < k:
+            return 0
+        elif k == len(self.products):
+            return self.product
+        else:
+            return int(self.products[-1] / self.products[-1 - k])
