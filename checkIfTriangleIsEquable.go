d1dbe336589978a7068c192e0d6456d02dffcebd
jdav892 <jadavila9@gmail.com>
2025-02-21 22:01:15 -0500

    feat: compares perimeter and area of a triangle and returns a boolean, specific to go and staticly typed languages and the importance of type casting

---
+++ checkIfTriangleIsEquable.go
@@ -0,0 +1,13 @@
+package kata
+
+import (
+	"math"
+)
+
+func EquableTriangle(a, b, c int) bool {
+	s := float64(a + b + c) / 2
+	perimeter := float64(a + b + c)
+	area =: math.Sqrt(s * (s - float64(a)) * (s - float64(b)) * (s - float64(c)))
+
+	return perimeter == area
+}
