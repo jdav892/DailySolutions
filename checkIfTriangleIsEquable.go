948bb6d716ded2349e4f6980489c5c78512bed23
jdav892 <jadavila9@gmail.com>
2025-02-21 22:01:38 -0500

    feat: fixed variable declaration

--- checkIfTriangleIsEquable.go
+++ checkIfTriangleIsEquable.go
@@ -5,9 +5,9 @@ import (
 )
 
 func EquableTriangle(a, b, c int) bool {
-	s := float64(a + b + c) / 2
+	s := float64(a+b+c) / 2
 	perimeter := float64(a + b + c)
-	area =: math.Sqrt(s * (s - float64(a)) * (s - float64(b)) * (s - float64(c)))
+	area := math.Sqrt(s * (s - float64(a)) * (s - float64(b)) * (s - float64(c)))
 
 	return perimeter == area
 }
