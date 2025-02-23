35044dab9e101d6a6b7e5b1564a4303a898c8129
jdav892 <jadavila9@gmail.com>
2025-02-23 13:58:58 -0500

    feat: implements a stack to store values, and performs a preorder traversal through the tree, while the stack is greater than dashes before values are appended, dashes are popped from the stack then checks for left and right values and returns the values from the tree

---
+++ recover_a_tree_from_preorder_traversal.py
@@ -0,0 +1,29 @@
+
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        dashes = 0
+        stack = []
+        i = 0
+        
+        while i < len(traversal):
+            if traversal[i] == "-":
+                dashes += 1
+                i += 1
+            else:
+                j = i
+                while j < len(traversal) and traversal[j] != "-":
+                    j += 1
+                
+                val = int(traversal[i:j])
+                node = TreeNode(val)
+                
+                if stack and not stack[-1].left:
+                    stack[-1].left = node
+                elif stack:
+                    stack[-1].right = node
+                stack.append(node)
+                i = j
+                dashes = 0
+        return stack[0]
+                    
+                    
