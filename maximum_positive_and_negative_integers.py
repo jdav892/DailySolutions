d82113a698600647e5233e21843e5375d0544d8c
jdav892 <jadavila9@gmail.com>
2025-03-12 05:06:43 -0400

    feat: implements a binary search to efficiently section off indexes and select from those within said range to then pass them through a helper function to return the max of the negative and positive values from each section of the nums array

--- maximum_positive_and_negative_integers.py
+++ maximum_positive_and_negative_integers.py
@@ -1,15 +1,20 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        positive = []
-        negative = []
-
-        for num in nums:
-            if num < 0:
-                negative.append(num)
-            elif num > 0:
-                positive.append(num)
-            else:
-                continue
+        neg = self.binary_search(nums, 0)
+        pos = len(nums) - self.binary_search(nums, 1)
+        return max(neg, pos)
+    
+    
+    def binary_search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        result = len(nums)
         
-        result = max(len(negative), len(positive))
-        return result
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                result = mid
+                right = mid - 1
+        
+        return result 
