8f19c50456b07d28ef41377468b0fe055a4b260a
jdav892 <jadavila9@gmail.com>
2025-03-14 16:10:34 -0400

    feat: implements a binary search through the sum of the candies given to denote a left and right section, then performs another binary search between those sections to then step through the candies given and assign a result to the value of the mid point, and return the result after the loop finishes

---
+++ maximum_candies_allocated_to_k_children.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        total = sum(candies)
+        if total < k:
+            return 0
+        
+        l, r = 1, sum(candies) // k
+        result = 0
+        
+        while l <= r:
+            m = (l + r) // 2
+            count = 0
+            
+            for c in candies:
+                if c >= m:
+                    count += c // m
+                if count >= k:
+                    break
+            if count >= k:
+                result = m
+                l = m + 1
+            else:
+                r = m - 1
+        return result
