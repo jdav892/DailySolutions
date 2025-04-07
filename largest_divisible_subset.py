9c0b8294fd6ac7b46096fd3d8a0b8a108d83fbeb
jdav892 <jadavila9@gmail.com>
2025-04-06 23:50:39 -0400

    feat: uses a depth first search and a cache to traverse the sorted values of the given list and return a result, then by dynamically calling the dfs function and when the length of the temporary variable is greater than the result the result is reassigned, and loop is finished return the result

---
+++ largest_divisible_subset.py
@@ -0,0 +1,24 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        cache = {}
+        
+        def dfs(i):
+            if i == len(nums): return []
+            if i in cache: return cache[i]
+            
+            result = [nums[i]]
+            for j in range(i + 1, len(nums)):
+                if nums[j] % nums[i] == 0:
+                    temp = [nums[i]] + dfs(j)
+                    if len(temp) > len(result):
+                        result = temp
+                        
+            cache[i] = result
+            return result 
+        
+        for i in range(len(nums)):
+            temp = dfs(i)
+            if len(temp) > len(result):
+                result = temp
+        return result
