6a35d0dc7cc99c143ca90575aea2297ec644467a
jdav892 <jadavila9@gmail.com>
2025-04-24 23:20:23 -0400

    feat: kinda late

---
+++ count_of_interest_subarrays.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
+        N = len(nums)
+        
+        prev = collections.Counter()
+        prev[0] = 1
+        total = 0
+        current = 0
+        degree = 1
+        
+        for i in range(N):
+            if nums[i] % modulo == k:
+                current += 1
+            total += prev[(current - k) % modulo]
+            prev[current % modulo] += 1
+        return total
