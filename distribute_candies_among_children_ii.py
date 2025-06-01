3bdf6d1344588d756193b2a21c5086b44cef3da4
jdav892 <jadavila9@gmail.com>
2025-06-01 17:25:09 -0400

    feat: increments total candies by max amount within constraints of given limit depending if it's greater than or equal vs less than and returns a total when the loop ends

---
+++ distribute_candies_among_children_ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        if limit * 3 < n:
+            return 0
+        
+        total = 0
+        for i in range(min(n + 1, limit + 1)):
+            candies_left = n - i
+            
+            if limit >= candies_left:
+                total += candies_left + 1
+            else:
+                total += max(limit - (candies_left - limit) + 1, 0)
+        return total
