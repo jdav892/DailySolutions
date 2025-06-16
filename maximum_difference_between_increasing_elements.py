786cde4acc9d1fd65bb624b47cc912e1fb622633
jdav892 <jadavila9@gmail.com>
2025-06-16 19:10:32 -0400

    feat: my initial solution by stepping through the array twice and assigning a diff, then returning it

---
+++ maximum_difference_between_increasing_elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        diff = 0
+        n = len(nums)
+        for i in range(0, n):
+            for j in range(1, n):
+                if nums[j] - nums[i] > diff and i < j:
+                    diff = nums[j] - nums[i]
+                else:
+                    continue
+        
+        if diff == 0:
+            return -1
+        else:
+            return diff
