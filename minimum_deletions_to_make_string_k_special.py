f147b701d157af0b8672efc4e57a9524669e14e8
jdav892 <jadavila9@gmail.com>
2025-06-21 18:42:48 -0400

    feat: another is saved from the maw

---
+++ minimum_deletions_to_make_string_k_special.py
@@ -0,0 +1,29 @@
+import collections
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        f = collections.Counter(word)
+        
+        v = list(sorted(f.values))
+        prefix = [0]
+        for x in v:
+            prefix.append(prefix[-1] + x)
+            
+        N = len(word)
+        best = N
+        smaller = 0
+        right = 0
+        
+        for left in range(len(v)):
+            if left - 1 >=  0:
+                smaller += v[left - 1]
+            right = max(right, left)
+            
+            while right < len(v) and v[right] - v[left] <= k:
+                right += 1
+            
+            bigger = (prefix[len(v)] - prefix[right]) - ((v[left] + k) * (len(v) - (right)))
+            
+            best = min(best, smaller + bigger)
+            
+        return best
