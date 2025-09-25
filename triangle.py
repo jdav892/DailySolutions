5c9034442118099af75d85c22814d3d634783a81
Justin Davila <Jadavila9@gmail.com>
2025-09-25 18:55:41 -0400

    feat: larry?

---
+++ triangle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        N = len(triangle)
+        INF = 10 ** 20
+
+        dp = [[triangle[0][0]]]
+
+        for i in range(1, N):
+            dp.append([])
+            for j in range(i + 1):
+                dp[i].append(0)
+
+                dp[i][j] = INF
+                if i - 1 >= 0 and j - 1 >= 0:
+                    dp[i][j] = min(dp[i][j], triangle[i][j] + dp[i - 1][j - 1])
+
+                if i - 1 >= 0 and j < i:
+                    dp[i][j] = min(dp[i][j], triangle[i][j] + dp[i - 1][j])
+        
+        return min(dp[-1])
