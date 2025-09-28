54be0a796ed51c9a90b75e9e236a8596ebbde7a7
Justin Davila <jadavila9@gmail.com>
2025-09-28 19:30:32 -0400

    feat: used tab complete for the first time?? also triangles are neat

---
+++ largest_perimeter_triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        
+        for c, b, a in zip(nums, nums[1:], nums[2:]):
+            if a + b > c:
+                return a + b + c
+        else:
+            return 0
