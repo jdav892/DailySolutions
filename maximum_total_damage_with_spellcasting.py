f31c59bd66d674243594a6e51930180fb4db8321
Justin Davila <jadavila9@gmail.com>
2025-10-11 19:16:54 -0400

    feat: using a counting dict for efficient look ups to then reassign best through a cached helper function that's called through every loop iteration

---
+++ maximum_total_damage_with_spellcasting.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        f = collections.Counter(power)
+        keys = list(sorted(f.keys()))
+        N = len(keys)
+        
+        @cache
+        def get_max(index):
+            if index >= N:
+                return 0
+            
+            best = get_max(index + 1)
+            
+            for i in range(1, 4):
+                if index + i < N and keys[index + i] > keys[index] + 2:
+                    best = max(best, get_max(index + i) + keys[index] * f[keys[index]])
+                    
+            best = max(best, keys[index] * f[keys[index]])
+            
+            return best
+        return get_max(0)
