b994eaa6d3cf0388d4e27be332fd80f38432b863
Justin Davila <jadavila9@gmail.com>
2025-10-29 02:06:33 -0400

    feat: cpp solution

--- SmallestNumberWithAllBitsSet.cpp
+++ SmallestNumberWithAllBitsSet.cpp
@@ -9,6 +9,6 @@ class Solution
                 if(pow(2, i) <= n) break;
             }
             cout << 1;
-            return pow(2, i + 1) - 1
+            return pow(2, i + 1) - 1;
         }
-}
+};
